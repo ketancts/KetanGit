0927af972b6ffeaaeaebe44454918aa618da02bb
Ketan <ketan@coretus.com>
2021-02-01 17:26:30 +0530

    View Binding

--- app/src/main/java/com/ketan/git/MainActivity.java
+++ app/src/main/java/com/ketan/git/MainActivity.java
@@ -2,12 +2,20 @@ package com.ketan.git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.ketan.git.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
     }
 }
